5795b8411ed90e2212cb3b192a1046f8b9db6d85
Derfel Cadarn <gehigbie@gmail.com>
2020-01-27 21:06:19 -0700

    Edit file

--- pass_length.py
+++ pass_length.py
@@ -2,8 +2,7 @@
 def find_pass_length():
     user_name = input('Please enter a username --> ')
     password = input('Please enter a password --> ')
-    pass_length = len(password)
 
-    print(f'{user_name}, your password is {"*" * pass_length} is {pass_length} letters long.')
+    print(f'{user_name}, your password is {"*" * len(password)} is {len(password)} letters long.')
 
 find_pass_length()
