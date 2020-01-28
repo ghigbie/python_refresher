57acb023b5733a31f8a666ae5ad80842efa7f3c7
Derfel Cadarn <gehigbie@gmail.com>
2020-01-27 20:27:23 -0700

    Add space for input

--- birth_year.py
+++ birth_year.py
@@ -1,7 +1,7 @@
 import datetime
 
 def find_age():
-    birth_year = int(input('What is your birth year?'))
+    birth_year = int(input('What is your birth year?  '))
     current_year = datetime.datetime.now().year
     print(current_year - birth_year)
 
