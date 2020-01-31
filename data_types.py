6d8a7a66e263d1a110e4e3995174e05de61bfab4
Derfel Cadarn <gehigbie@gmail.com>
2020-01-30 20:43:00 -0700

    Add new data structures methods

--- data_types.py
+++ data_types.py
@@ -16,6 +16,18 @@ print(set1)
 print(set2)
 print(set3)
 
+my_set = {1, 2, 3, 4, 5}
+your_set = {4, 5, 6, 7, 8, 9 , 10}
+
+print("difference: " + my_set.difference(your_set))
+print("discard: " + my_set.discard(4))
+print("difference_update: " + my_set.difference_update(your_set) ) #removes the different elements
+print("isdisjoint: " + my_set.intersection(your_set))
+print("issubset: " + my_set.issubset(your_set))
+print("union: " + my_set.union(your_set))
+
+
+
 #tuple - immutable lists
 tuple1 = (1,2,3,4,6,0,7)
 tuple2 = tuple(list1)
