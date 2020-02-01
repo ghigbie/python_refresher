791e4f5727d892b541a587e2aaaacdb68f69c358
Derfel Cadarn <gehigbie@gmail.com>
2020-01-31 20:52:43 -0700

    Edit function

--- data_types.py
+++ data_types.py
@@ -43,9 +43,10 @@ print(tuple2)
 new_list = [1,2,3,4,5,6,7,8,9,10]
 
 def sum_list(any_list):
-    for i in any_list:
-        i += i
-    return i
+    counter = 0
+    for item in any_list:
+        counter = counter + item
+    return counter
 
 sum = sum_list(new_list)
 print(sum)
