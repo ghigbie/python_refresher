524f925f623d2d3b0caab74d2c81d714799f1749
Derfel Cadarn <gehigbie@gmail.com>
2020-01-31 20:48:52 -0700

    Add new function for sum

--- data_types.py
+++ data_types.py
@@ -19,16 +19,16 @@ print(set3)
 my_set = {1, 2, 3, 4, 5}
 your_set = {4, 5, 6, 7, 8, 9 , 10}
 
-print("difference: " + my_set.difference(your_set))
-print("discard: " + my_set.discard(4))
-print("difference_update: " + my_set.difference_update(your_set) ) #removes the different elements
-print("intersection: " + my_set.intersection(your_set))
-print("intersection 2: "+ my_set & your_set)
-print("isdisjoint: " + my_set.intersection(your_set)) # do sets have something in common
-print("issubset: " + my_set.issubset(your_set))
-print("issuperset: " + my_set.issuperset(your_set))
-print("union: " + my_set.union(your_set))
-print("union2: " + my_set | your_set)
+# print("difference: " + my_set.difference(your_set))
+# print("discard: " + my_set.discard(4))
+# print("difference_update: " + my_set.difference_update(your_set) ) #removes the different elements
+# print("intersection: " + my_set.intersection(your_set))
+# print("intersection 2: "+ my_set & your_set)
+# print("isdisjoint: " + my_set.intersection(your_set)) # do sets have something in common
+# print("issubset: " + my_set.issubset(your_set))
+# print("issuperset: " + my_set.issuperset(your_set))
+# print("union: " + my_set.union(your_set))
+# print("union2: " + my_set | your_set)
 
 
 
@@ -39,3 +39,13 @@ tuple2 = tuple(list1)
 print("tuples")
 print(tuple1)
 print(tuple2)
+
+new_list = [1,2,3,4,5,6,7,8,9,10]
+
+def sum_list(any_list):
+    for i in any_list:
+        i += i
+    return i
+
+sum = sum_list(new_list)
+print(sum)
